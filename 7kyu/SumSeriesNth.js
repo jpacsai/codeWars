7455604c0aca3171c5185d9837e1d146a9c99c72
jpacsai <judit.pacsai@gmail.com>
2017-12-16 17:16:09 +0100

    fixed Kata

---
+++ 7kyu/SumSeriesNth.js
@@ -0,0 +1,20 @@
+/*
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+*/
+
+function SeriesSum(n) {
+  var sum = 0;
+  for(var i = 0; i < n; i++) {
+    sum += 1 / (3 * i + 1);
+  }
+  return sum.toFixed(2);
+}
