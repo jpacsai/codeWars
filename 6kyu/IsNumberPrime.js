5f7ea6e8c33605cef17c65db7fa71712b046ecae
jpacsai <judit.pacsai@gmail.com>
2017-12-19 22:03:10 +0100

    adding Kata

---
+++ 6kyu/IsNumberPrime.js
@@ -0,0 +1,25 @@
+/*
+Define a function isPrime/is_prime() that takes one integer argument and returns 
+true/True or false/False depending on if the integer is a prime.
+
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 
+that has no positive divisors other than 1 and itself.
+
+Example
+isPrime(5)
+=> true
+
+*/
+
+function isPrime(num) {
+    var m;
+    if (num < 2) {m = false;}
+    if (num === 2) {m = true;}
+    if (num > 2) {
+      m = true;
+      for (var x = 2; x < num; x++) {
+        if (num % x === 0){m = false;}
+      }
+    }
+    return m;
+}
