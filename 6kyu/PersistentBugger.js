139222b7589d261ecd06986888987f8d31793892
jpacsai <judit.pacsai@gmail.com>
2017-12-19 02:10:04 +0100

    addin Kata

---
+++ 6kyu/PersistentBugger.js
@@ -0,0 +1,21 @@
+/*
+Write a function, 'persistence', that takes in a positive parameter num and 
+returns its multiplicative persistence, which is the number of times you must 
+multiply the digits in num until you reach a single digit.
+
+*/
+
+function persistence(num) {
+    var count = 0;
+    var char = num.toString().length;
+    var sum = 1;
+  
+    while (char > 1) {
+      for (var y = 0; y < char; y++) {sum *= Number(num.toString().charAt(y));}
+      num = sum;
+      char = num.toString().length;
+      count += 1;
+      sum = 1;
+    }
+    return count;
+  }
