8baf22fc9445181c303d9cf95da03e5a2a189b66
jpacsai <judit.pacsai@gmail.com>
2017-12-21 19:39:32 +0100

    adding Kata

---
+++ 7kyu/MiddleElement.js
@@ -0,0 +1,16 @@
+/*
+As a part of this Kata, you need to create a function that when provided with a triplet, 
+returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+*/
+
+var gimme = function (inputArray) {
+    return inputArray.indexOf(inputArray.slice(0).sort(function(a, b){return a-b;})[1]);;
+  };
