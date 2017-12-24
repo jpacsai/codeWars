07689f1734f06ab4ba1978a96abbb17696580ad9
jpacsai <judit.pacsai@gmail.com>
2017-12-24 11:17:21 +0100

    finishing kata

--- 6kyu/TitleCase.js
+++ 6kyu/TitleCase.js
@@ -1,47 +1,36 @@
 /*
+A string is considered to be in title case if each word in the string is either 
+(a) capitalised (that is, only the first letter of the word is in upper case) or 
+(b) considered to be an exception and put entirely into lower case unless it is the first word, 
+which is always capitalised.
 
+Write a function that will convert a string into title case, given an optional list of exceptions 
+(minor words). The list of minor words will be given as a string with each word separated by a space.
+Your function should ignore the case of the minor words string -- it should behave in the same way 
+even if the case of the minor word string is changed.
 
 */
 
 function titleCase(title, minorWords) {
-    var titleStr = title.split(" ");
-    var minorStr = minorWords.split(" ");
-    var first = titleStr[0].charAt(0).toUpperCase().concat(titleStr[0].slice(1));
-    var newStr = first;
-    var a = titleStr.length;
-    for (var x = 1; x <= a; x++) {
-      var b = titleStr[x].length;
-      for (var y = 0; y <= b; y++) {
-        if (titleStr[x] == minorStr[y]) {
-          newStr.concat(" ", minorStr[y]);
-        }
-        else {
-          newStr.concat(" ", titleStr[0].charAt(0).toUpperCase().concat(titleStr[0].slice(1)));
-        }
-      }
-    }
-    return newStr;
-  }
-  
-  titleCase('a clash of KINGS', 'a an the of');
-  
-  console.log(titleCase);
-  
-  /*
-  var titleStr = ["a","clash","of","KINGS"];
-    var minorStr = ["a","an","the","of"];
-    var newStr = "A";
-    var a = 4;
-    for (var x = 1; x <= a; x++) {
-      var b = titleStr[x].length;
-      for (var y = 0; y <= b; y++) {
-        if (titleStr[x] == minorWords[y]) {
-          newStr.concat(" ", minorWords[y]);
-        }
-        else {
-          newStr.concat(" ", titleStr[0].charAt(0).toUpperCase().concat(titleStr[0].slice(1)));
-        }
-      }
+  var titleStr = title.split(" ");
+    for (var a = 0; a < titleStr.length; a++) {
+      titleStr[a] = titleStr[a].toLowerCase();
     }
   
-  */
+  var minorStr = minorWords.split(" ");
+    for (var b = 0; b < minorStr.length; b++) {
+      minorStr[b] = minorStr[b].toLowerCase();
+    }
+  
+  var newStr = titleStr[0].charAt(0).toUpperCase().concat(titleStr[0].slice(1));
+  
+    for (var x = 1 ; x < titleStr.length; x++) {
+      if (minorStr.includes(titleStr[x])) {
+        newStr = newStr.concat(" ",titleStr[x]);
+      }
+      else {
+        newStr = newStr.concat(" ",titleStr[x].charAt(0).toUpperCase().concat(titleStr[x].slice(1)));
+      }
+    }
+    return newStr; 
+}
