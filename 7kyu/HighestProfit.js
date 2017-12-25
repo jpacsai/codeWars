c4b382df652518d86fb4795a31c067f52f2850ae
jpacsai <judit.pacsai@gmail.com>
2017-12-26 00:07:45 +0100

    adding katas

---
+++ 7kyu/HighestProfit.js
@@ -0,0 +1,19 @@
+/*
+Story
+Ben has a very simple idea to make some profit: he buys something and sells it again. 
+Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. 
+Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples
+minMax([1,2,3,4,5])   == [1,5]
+minMax([2334454,5])   == [5, 2334454]
+minMax([1])           == [1, 1]
+
+*/
+
+function minMax(arr){
+    return [Math.min(...arr),Math.max(...arr)];
+}
---
+++ 7kyu/StrayNumber.js
@@ -0,0 +1,24 @@
+/*
+You are given an odd-length array of integers, in which all of them are the same, 
+except for one single number.
+
+Implement the method stray which accepts such array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples:
+
+[1, 1, 2] => 2
+
+[17, 17, 3, 17, 17, 17, 17] => 3
+
+*/
+
+function stray(numbers) {
+    var arr = numbers.sort();
+    if (arr[0] != arr[1]) {
+      console.log(arr[0]);
+      return arr[0];
+    }
+    return arr[arr.length-1];
+  }
