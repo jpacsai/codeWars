44dff29afa07abba0d2233713f5a9c4314182d35
jpacsai <judit.pacsai@gmail.com>
2018-01-02 00:47:35 +0100

    adding kata

---
+++ 7kyu/AllInclusive.js
@@ -0,0 +1,21 @@
+// All Inclusive?
+
+/*
+Input:
+
+a string strng
+an array of strings arr
+Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+a boolean true if all rotations of strng are included in arr (C returns 1)
+false otherwise (C returns 0)
+#Examples:
+
+contain_all_rots(
+  "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+contain_all_rots(
+  "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+
+*/
+
