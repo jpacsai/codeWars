723b948c9f926245475ca721b62c5eb18f1d2e4c
jpacsai <judit.pacsai@gmail.com>
2018-08-30 14:33:18 +0200

    add solved js kata

---
+++ 7kyu/TwoToOne.js
@@ -0,0 +1,18 @@
+// Two to One
+
+/*
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+each taken only once - coming from s1 or s2. #Examples: ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ```
+
+*/
+
+function longest(s1, s2) {
+    return s1.concat(s2).split('').sort().reduce((string, l) => {
+      if (string.includes(l) === false) {
+        string.push(l)
+      }
+      return string;
+    }, []).join('');
+}
