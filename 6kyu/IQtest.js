11ad4a099753b048219e8ea480e5adf4a08a0dfe
Judit Cs. Pacsai <34191321+jpacsai@users.noreply.github.com>
2018-08-31 19:39:42 +0200

    add solved js kata

---
+++ 6kyu/IQtest.js
@@ -0,0 +1,23 @@
+// IQ test
+
+/*
+Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+##Examples :
+
+iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+
+iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+
+*/
+
+function iqTest(numbers){
+    const arr = numbers.split(' ').map(n => n % 2);
+    for (let i = 0; i < arr.length - 2; i++) {
+      if (arr[i] !== arr[i+1] || arr[i+1] !== arr[i+2] ) {
+        return arr[i] === arr[i+1] ? i + 3 : arr[i] === arr[i+2] ? i + 2 : i + 1;
+      }
+    }
+}
