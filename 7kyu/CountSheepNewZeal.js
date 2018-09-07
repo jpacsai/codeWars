7e154b5ed4762716292af2255fa6bd2198b44544
jpacsai <judit.pacsai@gmail.com>
2018-09-07 12:16:00 +0200

    add solved js kata
    7kyu - Count all the sheep on farm in the heights of New Zealand
    (CountSheepNewZeal)

---
+++ 7kyu/CountSheepNewZeal.js
@@ -0,0 +1,23 @@
+// Count all the sheep on farm in the heights of New Zealand
+
+/*
+Every week (Friday and Saturday night), the farmer and his son count amount of sheep returned to the yard of their farm.
+
+They count sheep on Friday night, the same goes for Saturday (suppose that sheep returned on Friday are not feeding back on hills on Saturday).
+
+As sheep are not coming in one flock, you will be given two arrays (one for each night) representing number of sheep as they were returning to the yard during the evenings (entries are positive ints, higher than zero).
+
+Farmer and his son know the total amount of their sheep, you will be given this number as third parameter.
+
+Your goal is to calculate the amount of sheep lost (not returned) to the farm after Saturday night counting.
+
+Example 1: Input: {1, 2}, {3, 4}, 15 --> Output: 5
+
+Example 2: Input: {3, 1, 2}, {4, 5}, 21 --> Output: 6
+
+*/
+
+function lostSheep(friday,saturday,total){
+    const all = friday.concat(saturday);
+    return all.length ? total - all.reduce((total, sheep) => total += sheep) : total;
+}
