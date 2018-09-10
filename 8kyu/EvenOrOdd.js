7d3c84bbf34073e497d1595260f481b2185c984c
jpacsai <judit.pacsai@gmail.com>
2018-09-10 17:06:05 +0200

    add solved js kata: 8kyu - Even or Odd
    EvenOrOdd.js

---
+++ 8kyu/EvenOrOdd.js
@@ -0,0 +1,10 @@
+// Even or Odd
+
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+
+function even_or_odd(number) {
+    return number % 2 ? 'Odd' : 'Even'
+}
