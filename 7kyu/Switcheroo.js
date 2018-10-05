66ba5f80b5383ee021ea49a13ed2a8157bc0ceeb
jpacsai <judit.pacsai@gmail.com>
2018-10-05 11:42:30 +0200

    add solved js kata: 7kyu - Switcheroo
    Switcheroo.js

---
+++ 7kyu/Switcheroo.js
@@ -0,0 +1,15 @@
+// Switcheroo
+
+/*
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+
+*/
+
+function switcheroo(x){
+    return x.split('').map(char => char === 'a' ? 'b' : char === 'b' ? 'a' : 'c' ).join('');
+}
