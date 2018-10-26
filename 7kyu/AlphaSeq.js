103f5d411bd1c60250be57400f3adf13c1399146
jpacsai <judit.pacsai@gmail.com>
2018-10-26 22:55:09 +0200

    add solved js kata: 7kyu - Alphabetical Sequence
    AlphaSeq.js

---
+++ 7kyu/AlphaSeq.js
@@ -0,0 +1,20 @@
+// Alphabetical Sequence
+
+/*
+In this kata you will be given a random string of letters and tasked with returning them as a string of comma-separated sequences sorted alphabetcally, with each sequence starting with an uppercase character followed by n-1 lowercase characters, where n is the letter's alphabet position 1-26.
+
+Example
+alphaSeq("ZpglnRxqenU") -> "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"
+
+Technical Details
+The string will include only letters.
+The first letter of each sequence is uppercase followed by n-1 lowercase.
+Each sequence is seperated with a comma.
+Return value needs to be a string.
+
+*/
+
+function alphaSeq(str) {
+    return str.toLowerCase().split('').sort().map(c =>
+        c.toUpperCase() + c.repeat(c.charCodeAt(0) - 97)).join(',');
+}
