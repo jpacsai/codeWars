8dfb70626820721d3546cb41688c0ea71806e03e
jpacsai <judit.pacsai@gmail.com>
2018-10-26 20:08:30 +0200

    add solved js kata: 8kyu - Count of positives / sum of negatives
    CountPosSumNeg.js

---
+++ 8kyu/CountPosSumNeg.js
@@ -0,0 +1,22 @@
+// Count of positives / sum of negatives
+
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+If the input array is empty or null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+*/
+
+function countPositivesSumNegatives(input) {
+    if (input !== null && input.length > 0) {
+        const pos = input.filter(n => n > 0).length;
+        const neg = input.filter(n => n < 0).reduce((total, x) => total += x, 0);
+        return [pos, neg];
+    }
+    return [];
+}
