19dd13882a5c922fd73f2b9e13a27ca7f8287ddf
unknown <judit.pacsai@gmail.com>
2019-05-05 14:42:26 +0200

    add solved js kata: 7kyu - Complementary DNA
    ComplDNA

---
+++ 7kyu/ComplDNA.js
@@ -0,0 +1,20 @@
+// Complementary DNA
+
+/*
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here http://rosalind.info/problems/list-view/ (source)
+
+DNAStrand ("ATTGC") // return "TAACG"
+
+DNAStrand ("GTAT") // return "CATA" 
+*/
+
+function DNAStrand(dna){
+  const nucleobases = { T: 'A', A: 'T', G: 'C', C: 'G' }
+  return dna.split('').map(n => nucleobases[n]).join('');
+}
