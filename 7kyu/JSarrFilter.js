e23f5ad6acf84a1ae71976cb7dda72a80e19e79d
unknown <judit.pacsai@gmail.com>
2019-05-26 23:37:03 +0200

    add completed kata: 7kyu - JavaScript Array Filter
    JSarrFilter.js

---
+++ 7kyu/JSarrFilter.js
@@ -0,0 +1,12 @@
+// JavaScript Array Filter
+
+/*
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+*/
+
+const getEvenNumbers = numbersArray => numbersArray.filter(num => !(num % 2));
